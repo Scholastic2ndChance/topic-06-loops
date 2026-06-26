62abb7504c841cc5db4223e6031c859727b68731
Scholastic2ndChance <bmoore@edisonohio.edu>
2026-06-25 21:02:24 -0400

    Create solution.py
    Created the python file and pasted the starter code.

---
+++ solution.py
@@ -0,0 +1,18 @@
+# Topic 06 Collaborative Assignment
+# Your Name: Bradley Moore
+# Date: 25 June 2026
+
+# --- STARTER CODE ---
+# This function takes a number and returns its square.
+def square(n):
+    return n * n
+
+# This loop calls the function for numbers 1 through 5.
+for i in range(1, 6):
+    result = square(i)
+    print(i, "squared is", result)
+
+# --- YOUR EXTENSION BELOW THIS LINE ---
+# Ideas: Write a second function, change the range,
+# change what the function does, or add a while loop
+# that lets the user keep entering numbers until they type "quit".
